24a0f1524b32f1f0987099f6eabc451eedfdf35c
EgorNick <403851@edu.itmo.ru>
2024-09-16 19:21:07 +0300

    Change information of lists

--- app/src/main/java/com/example/myportfolio/ui/Information.kt
+++ app/src/main/java/com/example/myportfolio/ui/Information.kt
@@ -1,15 +1,18 @@
 package com.example.myportfolio.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun ShowInformation(projectName: String){
+fun ShowInformation(projectName: String, image: Int){
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -17,7 +20,8 @@ fun ShowInformation(projectName: String){
     ) {
         if (projectName.isNotBlank()) {
             Column {
-                Text(text = "Project: $projectName")
+                Text(text = "Project: ${projectName.toInt()}")
+                Image(painter = painterResource(image), contentDescription = " " )
             }
         } else {
             Text(text = "No project information available")
--- app/src/main/java/com/example/myportfolio/ui/Projects.kt
+++ app/src/main/java/com/example/myportfolio/ui/Projects.kt
@@ -3,14 +3,21 @@ package com.example.myportfolio.ui
 import com.example.myportfolio.R
 
 class Projects {
-    val listProjects: List<Pair<String, Int>> = listOf<Pair<String, Int>>(
-        "Information" to R.string.app_name,
-        "scaj" to R.string.app_name,
-        "Json" to R.string.app_name
+
+    val retrofit: Set<Pair<Int, Int>> = setOf<Pair<Int, Int>>(
+        R.string.app_name to R.drawable.img
+    )
+
+    val listProjects: List<Pair<String, Set<Pair<Int, Int>>>> =
+        listOf<Pair<String, Set<Pair<Int, Int>>>>(
+        "Information" to retrofit,
+        "scaj" to retrofit,
+        "Json" to retrofit
     )
     val listCourses: List<Pair<String, String>> = listOf<Pair<String, String>>(
         "Information" to "Retrofit",
         "scaj" to "Room",
         "Json" to "Json"
     )
+
 }
