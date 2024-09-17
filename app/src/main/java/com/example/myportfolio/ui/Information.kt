776053fa70163e3ea499689221de8d3ee44e46dd
EgorNick <403851@edu.itmo.ru>
2024-09-17 15:05:28 +0300

    Change information of lists

--- app/src/main/java/com/example/myportfolio/ui/Information.kt
+++ app/src/main/java/com/example/myportfolio/ui/Information.kt
@@ -6,10 +6,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.myportfolio.R
+import com.example.myportfolio.ui.theme.MyPortfolioTheme
 
 @Composable
 fun ShowInformation(projectName: String, image: Int){
@@ -19,12 +23,20 @@ fun ShowInformation(projectName: String, image: Int){
             .padding(16.dp)
     ) {
         if (projectName.isNotBlank()) {
-            Column {
+            Column(modifier = Modifier.align(alignment = Alignment.CenterHorizontally)) {
                 Text(text = stringResource(id = projectName.toInt()))
                 Image(painter = painterResource(image), contentDescription = " " )
             }
         } else {
             Text(text = "No project information available")
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StartPreview(){
+    MyPortfolioTheme {
+        ShowInformation(projectName = "hdhsjkfh", image = R.drawable.photo_portfolio)
     }
 }
